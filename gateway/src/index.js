1cb1dae7db851cf7ebf647632a7fd341e7d862a5
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-07-13 01:43:45 +0200

    Testing rabbitmq

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const axios = require("axios");
+const amqp = require("amqplib/callback_api");
+
 const app = express();
 const port = 5000;
 
@@ -11,15 +13,39 @@ app.get("/wilders", async (req, res) => {
     res.send(wilders.data);
   } catch (err) {
     console.log("an error occurred");
-    // console.log(err);
+    console.log(err);
     res.status(400).send("an error occurred");
   }
 });
 
 app.post("/wilder", async (req, res) => {
   try {
-    const wilders = await axios.post("http://wilders:5000/wilder/", req.body);
-    res.send(wilders.data);
+    // const wilders = await axios.post("http://wilders:5000/wilder/", req.body);
+    amqp.connect("amqp://queue", function (error0, connection) {
+      if (error0) {
+        throw error0;
+      }
+      connection.createChannel(function (error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+
+        var queue = "wilder";
+
+        channel.assertQueue(queue, {
+          durable: false,
+        });
+        channel.sendToQueue(queue, Buffer.from(JSON.stringify(req.body)));
+
+        console.log(" [x] Sent %s", req.body);
+      });
+      setTimeout(function () {
+        connection.close();
+        process.exit(0);
+      }, 500);
+    });
+
+    res.send("sent to queue");
   } catch (err) {
     console.log("an error occurred");
     // console.log(err);
